81a726a49c55c52b0156a3f4b0ae45310653bbaa
Koushik <paponkoushik@gmail.com>
2023-11-16 19:53:38 +0600

    vuex, vue router installed

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
+import axios from 'axios'
 import App from './App.vue'
+import VueRouter from "vue-router"
 
 Vue.config.productionTip = false
+axios.defaults.baseURL = "http://localhost:8000/api";
+Vue.prototype.$appURL = "http://localhost:8000"
+
+Vue.use(VueRouter)
 
 new Vue({
   render: h => h(App),
---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+export const routes = [
+  // {
+  //   path: '/',
+  //   name: 'login',
+  //   component: Login,
+  //   beforeEnter: (to, from, next) => {
+  //     if (store.getters['Auth/authenticated']) {
+  //       return next({
+  //         name: 'products'
+  //       });
+  //     }
+  //     next();
+  //   }
+  // }
+]
