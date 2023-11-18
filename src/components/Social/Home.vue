a81c0cd888c52205455fb4a5ff24897acddce232
Koushik <paponkoushik@gmail.com>
2023-11-18 12:26:47 +0600

    status and tweet section has been added

--- src/components/Social/Home.vue
+++ src/components/Social/Home.vue
@@ -1,34 +1,62 @@
 <template>
-  <header class="fixed-main-header">
-    <div class="tabs">
-      <div
-        :class="{ 'active-tab': activeTab === 'tab1' }"
-        @click="setActiveTab('tab1')"
-      >
-        For You
+  <div>
+    <header class="fixed-main-header">
+      <div class="tabs">
+        <div
+          :class="{ 'active-tab': activeTab === 'tab1' }"
+          @click="setActiveTab('tab1')"
+        >
+          For You
+        </div>
+        <div
+          :class="{ 'active-tab': activeTab === 'tab2' }"
+          @click="setActiveTab('tab2')"
+        >
+          Following
+        </div>
       </div>
-      <div
-        :class="{ 'active-tab': activeTab === 'tab2' }"
-        @click="setActiveTab('tab2')"
-      >
-        Following
+    </header>
+
+    <main>
+      <div class="post-section">
+        <textarea v-model="newTweet" placeholder="What's happening?"></textarea>
+        <button @click="postTweet">Post</button>
       </div>
-    </div>
-  </header>
+      <div v-if="activeTab === 'tab1'">
+        <tweets :tweets="forYouTweets" />
+      </div>
+      <div v-else-if="activeTab === 'tab2'">
+        <tweets :tweets="followingTweets" />
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
+import Tweets from "./Tweets.vue";
+
 export default {
   name: "Home",
+  components: {Tweets},
   data() {
     return {
       activeTab: 'tab1',
+      forYouTweets: [
+        { id: 1, username: 'User1', content: 'This is a tweet for you!' },
+      ],
+      followingTweets: [
+        { id: 1, username: 'User2', content: 'This is a tweet from someone you are following!' },
+      ],
+      newTweet: ''
     };
   },
   methods: {
     setActiveTab(tab) {
       this.activeTab = tab;
     },
+    postTweet() {
+      console.log('calling')
+    }
   },
 }
 </script>
@@ -40,6 +68,8 @@ export default {
   background-color: #ffffff;
   padding: 10px;
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  top: 0;
+  z-index: 1000;
 }
 
 .tabs {
@@ -52,10 +82,33 @@ export default {
 .tabs div {
   flex: 1;
   text-align: center;
+  padding: 10px 0;
+  font-weight: bold;
 }
 
 .active-tab {
   border-bottom: 2px solid #1da1f2;
   color: #1da1f2;
+}
+
+.post-section {
+  padding: 90px 10px 20px 10px;
+  border-bottom: 1px solid #ccc;
+}
+
+.post-section textarea {
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 10px;
+  border-radius: 8px;
+}
+
+.post-section button {
+  background-color: #1da1f2;
+  color: #fff;
+  border: none;
+  padding: 8px 16px;
+  cursor: pointer;
+  border-radius: 4px;
 }
 </style>
---
+++ src/components/Social/Tweets.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="tweet-container">
+    <div class="tweet-username">Koushik</div>
+    <div v-for="tweet in tweets" :key="tweet.id" class="tweet-card">
+      {{ tweet.content }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tweets",
+  props: ['tweets']
+}
+</script>
+
+<style scoped>
+.tweet-container {
+  padding: 10px;
+}
+
+.tweet-card {
+  border: 1px solid #ccc;
+  padding: 16px;
+  margin-top: 10px;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.tweet-username {
+  font-weight: bold;
+  margin-bottom: 8px;
+}
+</style>
